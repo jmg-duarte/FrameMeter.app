e1dedea5efb573fdca64f39282ad30ac44407eb4
José Duarte <duarte.gmj@gmail.com>
2025-01-17 18:09:53 +0000

    Initial Commit

---
+++ FrameMeter/FrameMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameMeterApp.swift
+//  FrameMeter
+//
+//  Created by José Duarte on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FrameMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
