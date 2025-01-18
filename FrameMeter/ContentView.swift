eb4e95a21a1731f1215b7945abcf94516fe12c78
José Duarte <duarte.gmj@gmail.com>
2025-01-18 12:37:32 +0000

    feat: add text and number of seconds

--- FrameMeter/ContentView.swift
+++ FrameMeter/ContentView.swift
@@ -12,23 +12,47 @@ struct ContentView: View {
     @State private var timer: Timer? // Timer to control the sequence
     @State private var isRunning = false // Button state
 
+    @State private var totalCycles = 0
+    @State private var activeCycle = -1
+    
     let totalRectangles = 24
+    let totalRectanglesCycles = 12
     let interval = 1.0 / 24.0 // Time interval between rectangle updates
 
     var body: some View {
         VStack {
-            Text("\(activeIndex + 1)")
-            
-            LazyVStack(spacing: 4) {
-                ForEach(0 ..< totalRectangles, id: \.self) { index in
-                    Rectangle()
-                        .fill((
-                            index <= activeIndex) ? Color.blue : Color.gray)
-                        .frame(height: 20)
-                        // .animation(.linear(duration: interval), value: activeIndex)
+            HStack {
+                Text("\(totalCycles)")
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                Text("+")
+                Text("\(activeIndex + 1)")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }.font(.system(size: 24, design: .monospaced))
+
+            HStack {
+                LazyVStack(spacing: 4) {
+                    ForEach(0 ..< totalRectanglesCycles, id: \.self) { index in
+                        Rectangle()
+                            .fill((
+                                index <= activeCycle) ? Color.blue : Color.gray)
+                            .frame(height: 45)
+                            // .animation(.linear(duration: interval), value: activeIndex)
+                    }
                 }
+                .padding(.bottom)
+                
+                LazyVStack(spacing: 4) {
+                    ForEach(0 ..< totalRectangles, id: \.self) { index in
+                        Rectangle()
+                            .fill((
+                                index <= activeIndex) ? Color.blue : Color.gray)
+                            .frame(height: 20)
+                            // .animation(.linear(duration: interval), value: activeIndex)
+                    }
+                }
+                .padding(.bottom)
             }
-            .padding()
+            
 
             HStack {
                 Button(action: {
@@ -51,6 +75,7 @@ struct ContentView: View {
                     withAnimation(.none) {
                         stopChronometer()
                         activeIndex = -1
+                        totalCycles = 0
                     }
                 }) {
                     Text("Reset")
@@ -61,8 +86,8 @@ struct ContentView: View {
                         .cornerRadius(10)
                 }
             }
-            .padding(.horizontal)
         }
+        .padding(.horizontal)
     }
     
     func toggleChronometer() {
@@ -75,8 +100,16 @@ struct ContentView: View {
 
     func startChronometer() {
         isRunning = true
-        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { t in
-            activeIndex = activeIndex == totalRectangles ? -1 : activeIndex + 1
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { _ in
+            if activeIndex == totalRectangles {
+                totalCycles += 1
+                activeIndex = -1
+                
+                activeCycle = (activeCycle + 1) % totalRectanglesCycles
+                
+            } else {
+                activeIndex += 1
+            }
         }
     }
 
